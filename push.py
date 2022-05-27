045f2b7680e79c60c191a4c2915cafd9d22fbe9b
Jack Joas <atharvavyas30@gmail.com>
2022-05-27 19:57:45 +0530

    update for main branch

--- push.py
+++ push.py
@@ -1,40 +1,22 @@
 import os
 import time
-from venv import main
 
-branch = "master"
+branch = "main"
 origin = "https://github.com/elit30/c43.git"
 dir = '/mk0'
 
-if branch == "main":
-    os.chdir(os.getcwd() + dir)
-    time.sleep(5)
-    os.system('git init')
-    time.sleep(5)
-    os.system('git add .')
-    time.sleep(5)
-    os.system('git commit -m ' + str(int(time.time())))
-    time.sleep(5)
-    os.system('git branch -M main')
-    time.sleep(5)
-    os.system('git remote add origin ' + origin)
-    time.sleep(5)
-    os.system('git push -u origin main')
-    time.sleep(5)
-
-if branch == "master":
-    os.chdir(os.getcwd() + dir)
-    time.sleep(5)
-    os.system('git init')
-    time.sleep(5)
-    os.system('git add .')
-    time.sleep(5)
-    os.system('git commit -m ' + str(int(time.time())))
-    time.sleep(5)
-    os.system('git remote add origin ' + origin)
-    time.sleep(5)
-    os.system('git push origin ' + branch)
-    time.sleep(5)
+os.chdir(os.getcwd() + dir)
+time.sleep(5)
+os.system('git init')
+time.sleep(5)
+os.system('git add .')
+time.sleep(5)
+os.system('git commit -m ' + str(int(time.time())))
+time.sleep(5)
+os.system('git remote add origin ' + origin)
+time.sleep(5)
+os.system('git push -u origin ' + branch)
+time.sleep(5)
 
 # git init
 # git add .
