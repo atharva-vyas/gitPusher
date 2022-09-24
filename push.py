e13f486ad39e701c236e2f1cf35c2151689d51e1
elit30 <85245060+elit30@users.noreply.github.com>
2022-09-24 21:37:40 +0530

    Update push.py

--- push.py
+++ push.py
@@ -1,14 +1,16 @@
 import os
 import time
 
-branch = "master"
+gitName = "gitPusher"
 dir = 'gitPusher'
 
-origin = "https://github.com/elit30/" + str(dir) +".git"
+branch = "master"
+
+origin = "https://github.com/elit30/" + str(gitName) +".git"
 os.chdir(os.getcwd() + "/" + dir)
 
 os.system('git init')
 os.system('git add .')
 os.system('git commit -m ' + str(int(time.time())))
 os.system('git remote add origin ' + origin)
-os.system('git push -u origin ' + branch)
+os.system('git push -u origin ' + branch + '--force')
