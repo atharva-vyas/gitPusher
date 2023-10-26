017e8fd2e3983e1e0bf2ca813dcac4955ce12987
Atharva Vyas <85245060+atharva-vyas@users.noreply.github.com>
2023-10-26 18:30:07 -0500

    Update push.py

--- push.py
+++ push.py
@@ -1,15 +1,49 @@
 import os
 import time
 
-gitName = "temporaryRepo"
-dir = "temporaryRepo"
+gitName = "personal"
+dir = '/personal/home/'
 
 branch = "master"
 
 origin = "https://github.com/atharva-vyas/" + str(gitName) +".git"
-os.chdir(os.getcwd() + "/" + dir)
+os.chdir(dir)
 
-os.system('git init')
+def gitReInit():
+    print('Do you want to re-initialize git repo? y/n')
+    print('WARNING: selecting yes will result in deleting all the previous git branches')
+    deleteGitDirectory0 = input("=>")
+    if deleteGitDirectory0 == 'y':
+        os.system('find "' + dir + '" -type d -name ".git" -exec rm -rf {} \;')
+        os.system('git init')
+        # os.system('find "$start_directory" -type d -name ".git" -exec rm -rf {} \;')
+    elif deleteGitDirectory0 == 'n':
+        pass
+    else:
+        print('invalid option')
+        gitReInit()
+gitReInit()
+
+print('')
+
+def gitInit():
+    print("this will push the latest version of /personal directory to your git repository")
+    print("Choose no if you dont know what to do, git init will wipe out all previous branches")
+    gitInit0 = input("git init? y/n \n=> ")
+
+    if gitInit0 == 'y':
+        os.system('git init')
+    elif gitInit0 == 'n':
+        pass
+    else:
+        os.system('clear')
+        print('invalid option')
+        gitInit()
+# gitInit()
+
+print('')
+
+os.system('find "' + dir + '" -type d -name "node_modules" -exec rm -rf {} \;')
 os.system('git add .')
 os.system('git commit -m ' + str(int(time.time())))
 os.system('git remote add origin ' + origin)
